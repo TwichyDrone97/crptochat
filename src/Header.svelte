8882b7292a1a16faa4cb00cb8db25ece8975d915
TwichyDrone97 <nolraj7@gmail.com>
2022-06-23 11:58:36 +0800

    first commit

---
+++ src/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1>CRYPTO CHANNEL</h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Hello doggo! <strong>{$username}</strong></span>
+      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+    {:else}
+
+      <h3>crypto is life!</h3>
+  {/if}
+</header>
